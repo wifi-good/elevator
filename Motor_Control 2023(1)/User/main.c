19bc05d7e7b49d4a0b410e703652217385550ece
wifi信号较强 <1487993012@qq.com>
2023-12-17 17:47:05 +0800

    mission finished ^_^

--- Motor_Control 2023(1)/User/main.c
+++ Motor_Control 2023(1)/User/main.c
@@ -7,7 +7,7 @@ OPENLOOPMODE:   LED常亮
 SPEEDMODE:      LED慢闪烁
 POSITIONMODE:   LED快闪烁
 */
-
+int TFlag=1;
 int NumFloor = 0;//to check the floor
 int KeyFlag = 0;//to show the value of key
 int KeyFloor = 2;//KeyFloor-1 is the floor to the key
@@ -33,6 +33,24 @@ void SpeedModeKEEP_Init(void)
 	duty=0;
 }
 
+void POSModeUP_Init(void)
+{
+	positionPIDStructure.ref = (refVoltage - 1.65f) * 50.f;
+	speedPIDStructure.ref =PID_Calc(&positionPIDStructure, position);
+	duty = -PID_Calc(&speedPIDStructure, speed);
+}
+
+void POSModeDOWN_Init(void)
+{
+	positionPIDStructure.ref = (refVoltage - 1.65f) * 50.f;
+	speedPIDStructure.ref = PID_Calc(&positionPIDStructure, position);
+	duty = PID_Calc(&speedPIDStructure, speed);
+}
+
+void POSModeKEEP_Init(void)
+{
+	duty = 0;
+}
 
 int Direction(int KeyFloor,int NumFloor)
 {
@@ -64,15 +82,15 @@ int main(void)
 	ADC1_Init();//adc初始化
 	TIM3_Encoder_Init();//TIM3编码器模式初始化
 	
-	PID_Init(&positionPIDStructure,0, 0, 0, 0, 0.001,-5000, 5000, 0);//参考一下pid.c了解这几个参数的意义。
-	PID_Init(&speedPIDStructure,0, 10.3, 6.89, 0, 0.001,-0.98, 0.98, 0);//  提示！！当加入电流环时需要修改速度环-0.98与0.98，可以参考pid的过程以及具体代码思考一下应改为多少？
+	PID_Init(&positionPIDStructure,1000, 400, 0, 0, 0.001,-5000, 5000, 0);//参考一下pid.c了解这几个参数的意义。
+	PID_Init(&speedPIDStructure,10.3, 6.8, 0, 0, 0.001,-0.98, 0.98, 0);//  提示！！当加入电流环时需要修改速度环-0.98与0.98，可以参考pid的过程以及具体代码思考一下应改为多少？
 	PID_Init(&IPIDStructure,0, 1, 0, 0, 0.001,-0.98, 0.98, 0);//
 	TIM1_PWM_Init(3599,0);//TIM1输出初始化。不分频。PWM频率=72000/(3599+1)=20kHz 
 	TIM2_Int_Init(7199,10);//TIM2中断初始化。中断频率=72000000/10/7200=1kHz
 	LED_Init();
 	KEY_Init();
 	LSE_Init();
-	NumFloor=Check_Floor();//Initilize the floor
+	NumFloor=1;//Initilize the floor
   while(1)
 	{
 		static u16 count=0;//LED显示计数
@@ -80,7 +98,7 @@ int main(void)
 		delay_ms(1);
 		debugData[0] = position;
 		debugData[1] = positionPIDStructure.ref;
-		debugData[2] = speedPIDStructure.ref;
+		debugData[2] = PID_Calc(&positionPIDStructure, position);//speedPIDStructure.ref;
 		debugData[3] = speed;
 		debugData[4] = duty;
 		debugData[5] = Ivalue;
@@ -93,7 +111,12 @@ int main(void)
 		Debug_SendData(debugData, 11);
 		
 		
-		KeyFloor = Check_KeyFloor();
+		KeyFlag = KEY_Scan(0);
+		
+		if(KeyFlag==KEY1_PRES)KeyFloor=KEY1_PRES;
+		if(KeyFlag==KEY2_PRES)KeyFloor=KEY2_PRES;
+		if(KeyFlag==KEY3_PRES)KeyFloor=KEY3_PRES;
+		if(KeyFlag==KEY4_PRES)KeyFloor=KEY4_PRES;
 		
 		LSE_Stop();
 		
@@ -105,10 +128,10 @@ int main(void)
 			PID_Clear(&positionPIDStructure, 0);
 			position = 0;
 		}
-		else if(KeyFlag==KEY1_PRES||KeyFlag==KEY2_PRES||KeyFlag==KEY3_PRES||KeyFlag==KEY4_PRES)
-		{
-			mode=SPEEDMODE;
-		}
+//		else if(KeyFlag==KEY1_PRES||KeyFlag==KEY2_PRES||KeyFlag==KEY3_PRES||KeyFlag==KEY4_PRES)
+//		{
+//			mode=SPEEDMODE;
+//		}
 		
 		//LED指示工作模式
 		if(mode == STOPMODE)
@@ -149,14 +172,7 @@ void mainControl(void)	//tim2定时器，频率1kHz
 	refVoltage = GetADC1Voltage();
 	
 	
-	if(NumFloor==2&&KeyFloor==1)
-	{
-		SpeedModeDOWN_Init();
-		delay_ms(2000);// this parameter is uncertain
-		SpeedModeKEEP_Init();
-		NumFloor = Check_Floor();
-		mode = STOPMODE;
-	}
+	
 	
 	if(mode == STOPMODE)
 	{
@@ -169,15 +185,50 @@ void mainControl(void)	//tim2定时器，频率1kHz
 	else if(mode == SPEEDMODE)//以下速度环程序是未加入电流环的，如果需要加入电流环，请参考讲义自行增改。
 	{
 		if(Direction(KeyFloor,NumFloor)==UP) SpeedModeUP_Init();
-		else if(Direction(KeyFloor,NumFloor)==DOWN) SpeedModeDOWN_Init();
+		else if(Direction(KeyFloor,NumFloor)==DOWN)
+		{
+			if(NumFloor==2&&KeyFloor==2)
+			{
+				SpeedModeDOWN_Init();
+				TFlag++;
+				if(TFlag%3300==0)
+				{
+					TFlag=1;
+					NumFloor=1;
+					duty=0;
+					mode = STOPMODE;
+				}
+				
+			}
+			else SpeedModeDOWN_Init();
+		}
+			
 		else if(Direction(KeyFloor,NumFloor)==KEEP) SpeedModeKEEP_Init();
 	}
 	else if(mode == POSITIONMODE)//以下位置环程序是未加入电流环的，如果需要加入电流环，请参考讲义自行增改。
 	{
-		positionPIDStructure.ref = (refVoltage - 1.65f) * 50.f;
-		speedPIDStructure.ref = PID_Calc(&positionPIDStructure, position);
-		duty = PID_Calc(&speedPIDStructure, speed);
+		if(Direction(KeyFloor,NumFloor)==UP) POSModeUP_Init();
+		else if(Direction(KeyFloor,NumFloor)==DOWN)
+		{
+			if(NumFloor==2&&KeyFloor==2)
+			{
+				POSModeDOWN_Init();
+				TFlag++;
+				if(TFlag%4000==0)
+				{
+					TFlag=1;
+					NumFloor=1;
+					duty=0;
+					mode = STOPMODE;
+				}
+				
+			}
+			else POSModeDOWN_Init();
+		}
+			
+		else if(Direction(KeyFloor,NumFloor)==KEEP) POSModeKEEP_Init();
 	}
+	
 	
 	
 	PWM_Modulation(duty);//PA8为TIM1_CH1 PA9为TIM1_CH2 最终效果是互补输出
